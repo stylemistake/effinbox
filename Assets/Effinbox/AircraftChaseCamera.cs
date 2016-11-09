bc8d71748fecffddd8159544d4d1889ce7d3869f
Aleksej Komarov <stylemistake@gmail.com>
2016-11-09 03:28:55 +0200

    Add EF2000 and custom tune shaders

--- Assets/Effinbox/AircraftChaseCamera.cs
+++ Assets/Effinbox/AircraftChaseCamera.cs
@@ -6,6 +6,9 @@ public class AircraftChaseCamera : MonoBehaviour {
 	public AircraftPhysics target;
     private new Transform camera;
 
+    public float height;
+    public float distance;
+
     // Use this for initialization
     public void Start() {
 		camera = GetComponent<Transform>();
@@ -16,10 +19,11 @@ public class AircraftChaseCamera : MonoBehaviour {
 		var targetPos = target.transform.position;
 		var targetVel = target.GetComponent<Rigidbody>().velocity;
 		var targetTfm = target.transform;
-		var cameraOffset = - targetVel.normalized * 0 - targetTfm.forward * 25;
+		var cameraOffset = - targetVel.normalized * distance / 10
+            - targetTfm.forward * distance;
         camera.position = targetPos + cameraOffset;
 		camera.rotation = Quaternion.LookRotation(targetTfm.forward, targetTfm.up);
-		transform.Translate(Vector3.up * 8);
+		transform.Translate(Vector3.up * height);
 	}
 
 }
